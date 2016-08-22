cb74d6873023f3758f747f8744c6a21093ef3e0f
Kyle Machulis <kyle@nonpolynomial.com>
2016-08-21 20:12:33 -0700

    build: Removing unused build file

--- build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=dylib={}", "rust-icon");
-    println!("cargo:rustc-link-search=native={}", "c:\\Users\\qdot\\code\\git-projects\\systray-rs\\resources\\");
-}
