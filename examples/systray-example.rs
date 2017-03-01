9830ae079f9e1bdb6ad6cb3e88093d6882322921
Kyle Machulis <kyle@nonpolynomial.com>
2017-02-28 23:59:56 -0800

    chore: Clean up warnings in example

--- examples/systray-example.rs
+++ examples/systray-example.rs
@@ -5,24 +5,24 @@ fn main() {
     let mut app;
     match systray::Application::new() {
         Ok(w) => app = w,
-        Err(e) => panic!("Can't create window!")
+        Err(_) => panic!("Can't create window!")
     }
     // w.set_icon_from_file(&"C:\\Users\\qdot\\code\\git-projects\\systray-rs\\resources\\rust.ico".to_string());
     // w.set_tooltip(&"Whatever".to_string());
-    app.set_icon_from_file(&"/usr/share/gxkb/flags/ua.png".to_string());
-    app.add_menu_item(&"Print a thing".to_string(), |window| {
+    app.set_icon_from_file(&"/usr/share/gxkb/flags/ua.png".to_string()).ok();
+    app.add_menu_item(&"Print a thing".to_string(), |_| {
         println!("Printing a thing!");
-    });
+    }).ok();
     app.add_menu_item(&"Add Menu Item".to_string(), |window| {
-        window.add_menu_item(&"Interior item".to_string(), |window| {
+        window.add_menu_item(&"Interior item".to_string(), |_| {
             println!("what");
-        });
-        window.add_menu_separator();
-    });
-    app.add_menu_separator();
+        }).ok();
+        window.add_menu_separator().ok();
+    }).ok();
+    app.add_menu_separator().ok();
     app.add_menu_item(&"Quit".to_string(), |window| {
         window.quit();
-    });
+    }).ok();
     println!("Waiting on message!");
     app.wait_for_message();
 }
